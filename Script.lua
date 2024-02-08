c40db463b05754ed21cb6a627cdbd9e248d0864e
Zogbovoh <159146202+Zogbovoh@users.noreply.github.com>
2024-02-08 14:17:26 +0100

    Add files via upload

--- Script.lua
+++ Script.lua
@@ -1,12 +1,3 @@
---[[
-The following functions have been exposed to lua:
-setBackground(string aPath) sets the background to the texture in the folder textures.
-createButton(string area name which the player enters, string context); adds a button to the current screen
-createTextfield(string context); adds a textfield to the top of the screen.
-CLS(); clears the screen.
-exitGame(); exits the game.
-playSound(string path to sound)
-]]--
 
 broodje = false;
 OV = false;

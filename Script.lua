b98a64a881c198d3842b0e009051c6e531ac9fe0
Zogbovoh <159146202+Zogbovoh@users.noreply.github.com>
2024-02-08 11:12:40 +0100

    Add files via upload

--- Script.lua
+++ Script.lua
@@ -15,47 +15,80 @@ function story(aName)
 	if(aName == "start") then
 		CLS()
 	    setBackground("lelystadcentrum.jpg")
-	    createTextfield("Je zit in lelystad en probeert over te stappen, maar je kan je OV-kaart niet vinden")
-	    createButton("eind", "Loop helemaal naar huis")
-		createButton("straat", "ga de stad in")
+		playSound("dramatic.wav")
+	    createTextfield("Je komt aan in lelystad en probeert over te stappen, maar je kan je OV-kaart niet vinden. Je ziet dat een zwerver jouw kaart oppakt en vertrekt. Je gaat hem achterna.")
+	    createButton("exit", "Loop helemaal naar huis")
+		createButton("straat", "Ga de stad in")
     end
     if(aName == "straat") then
 	    CLS()
         setBackground("jpeg.jpeg")
+		playSound("crowd.wav")
 		createTextfield("Je bent in het centrum van Lelystad. Waar ga je heen?")
-		createButton("Steeg", "Loop het steegje in")
+		createButton("steeg", "Loop het steegje in")
 		createButton("Subway", "Ga naar de subway")
+		createButton("station", "Ga naar het station")
 	end
-	if(aName == "Steeg") then
+	if(aName == "steeg") then
 	    CLS()
 	    setBackground("steeg.jpeg")
-	    createTextfield("Aan het eind van het steegje zie je een oude magere zwerver")
-		createButton("zwerver", "loop naar de zwerver toe")
+	    createTextfield("Aan het eind van het steegje zie je de zwerver die jouw kaart heeft")
+		createButton("zwerver", "Loop naar de zwerver toe")
     end
 	if (aName == "zwerver") then
+	    CLS()
 	    setBackground("zwerver.jpg")
+		playSound("ghoul-urgh.wav")
 	    createTextfield("Sorry, heb je iets wat ik kan eten? Ik geef je iets waardevols terug")
-		createButton("straat", "ik heb niks bij me sorry")
+		createButton("straat", "Ik heb niks bij me, sorry")
 		createButton("blij", "Ik heb een broodje voor je")
 	end
 	if (aName == "blij") then
+	   CLS()
+	if (broodje == true) then
 	    setBackground("blijezwerver.jpg")
-		createTextfield("Dankjewel voor dit broodje! ik heb deze OV-kaart gevonden maar jij mag hem wel hebben.")
-		createButton("straat" "loop terug naar het centrum")
-		OV = true;
+		createTextfield("Dankjewel! ik heb deze OV-kaart gevonden maar jij mag hem wel hebben.")
+		createButton("straat", "Loop terug naar het centrum")
+		   OV = true;
+	else
+	CLS()
+		setBackground("sippezwerver.jpg")
+		createTextfield("Maar je hebt helemaal geen eten bij je. Probeer je mij in de maling te nemen?")
+		createButton("straat", "Loop het steegje uit")
+	end
 	end
 	if(aName == "Subway") then
 	    CLS()
 		setBackground("Subway1.jpeg")
+		playSound("subway.wav")
 		createTextfield("Hey ben je van ubereats? Ik heb een ansjovies met kaas sub liggen.")
-		createButton("dat vieze broodje is niet van mij")
-		createButton("broodje" "Ja?")
+		createButton("straat", "Dat vieze broodje is niet van mij")
+		createButton("broodje", "Ja?")
     end
 	if(aName == "broodje") then
 	   CLS()
 	   setBackground("subway2.jpg")
+	   playSound("cash.wav")
 	   createTextfield("Hier is je bestelling")
-	   createButton("straat" "loop naar buiten")
+	   createButton("straat", "Loop naar buiten")
 	   broodje = true;
     end
+	if(aName == "station") then
+	   CLS()
+	   if (OV == true) then
+		   setBackground("stationlelystad.jpg")
+		   playSound("wee.wav")
+		   createTextfield("Je bent net op tijd voor de trein naar huis!")
+		   createButton("exit", "Je stapt in de trein en relax tot je thuis komt")
+		else
+		   CLS()
+		   setBackground("poortjes.jpg")
+		   playSound("trein.wav")
+		   createTextfield("Je kan niet verder zonder een OV-kaart")
+		   createButton("straat", "Ga terug naar de stad")
+		end
+    end
+	if(aName == "exit") then
+	     exitGame();
+	end
 end
